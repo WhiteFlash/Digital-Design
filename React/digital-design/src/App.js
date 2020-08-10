8bab05c90713a0c3fcbe34c8f62e8b84044afded
whiteFlash <ivankotov375@gmail.com>
2020-08-10 17:57:49 +0300

    Entry point for the app. Fucntion return main page. All components has to be inserted here.

--- React/digital-design/src/App.js
+++ React/digital-design/src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+
+import Test from './test';
+import Navigation from './navigation/navigation';
+import LeftCard from './left/left';
+import Rightimgwithtext from './rightimgwithtext/rightimgwithtext';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div className="container">
+        <h2 id="webpagetitle" className="col"> Flexbox Layout </h2>
+      </div>
+      <br></br>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Navigation></Navigation>
+          </div>          
+        </div>
+        <br></br>
+        <div className="row">
+          <div className="col">
+            <LeftCard></LeftCard>
+          </div>
+          <div className="col">
+              <Rightimgwithtext></Rightimgwithtext>
+          </div>
+        </div>
+        <div className="row" id="leftdives">
+          <div className="col">            
+              <p>1-st p</p>              
+              <p>2-st p</p>
+              <p>3-st p</p>            
+          </div>          
+        </div>
+        <div className="col">
+            <p>Test 1</p>
+            <p>Test 2</p>
+            <p>Test 3</p>
+            <Test></Test>
+          </div>
+      </div>
     </div>
   );
 }
