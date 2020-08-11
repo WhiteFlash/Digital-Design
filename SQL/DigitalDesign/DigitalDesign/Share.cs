1344d2f166c723155b7ea229dc9d89d98500a109
whiteFlash <ivankotov375@gmail.com>
2020-08-11 20:00:21 +0300

    Model of serializseble object from XML.

---
+++ SQL/DigitalDesign/DigitalDesign/Share.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalDesign
+{
+
+    [Serializable]
+    public class Share : IShare
+    {
+        public string BroadID { get; set; }
+        public string SecID { get; set; }
+        public string Shortname { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+
+        public Share() { }
+
+        public Share(string broadid, string secid, string shortname, decimal open, decimal close)
+        {
+            BroadID = broadid;
+            SecID = secid;
+            Shortname = shortname;
+            Open = open;
+            Close = close;
+        }
+    }
+}
