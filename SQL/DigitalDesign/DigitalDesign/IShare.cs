da4ca25dbb4757f80bd73dcc8e7303db6e6a3fe4
whiteFlash <ivankotov375@gmail.com>
2020-08-11 19:59:51 +0300

    Interface for Model.

---
+++ SQL/DigitalDesign/DigitalDesign/IShare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalDesign
+{
+    interface IShare
+    {
+        public string BroadID { get; set; }
+        public string SecID { get; set; }
+        public string Shortname { get; set; }
+        public decimal Open { get; set; }
+        public decimal Close { get; set; }
+    }
+}
