1f31761dd53614fa5087fb2ca241203b2d95143a
whiteFlash <ivankotov375@gmail.com>
2020-08-12 10:00:36 +0300

    Top 10 companies whose shares has risen output on the screen.

--- SQL/DigitalDesign/DigitalDesign/Program.cs
+++ SQL/DigitalDesign/DigitalDesign/Program.cs
@@ -16,7 +16,7 @@ namespace DigitalDesign
         ///     Persentage shows the difference betwin start of trade and the end.
         ///     Flag shows status. If flag is 1 then share has risen, if -1 then fall, 0 means no changes.
         /// </returns>
-        private static (decimal, sbyte) Calculate(decimal open, decimal closed)
+        private static (decimal, sbyte) CalculateShares(decimal open, decimal closed)
         {
             decimal percent; sbyte flag;
 
@@ -36,11 +36,40 @@ namespace DigitalDesign
             }           
             return (percent, flag);
         }
+        
+        private static void CustomSortArray(Share[] sharesArray)
+        {
+            Share temp;
+            // преобразования, чтобы от null избавиться в массиве. Надо потом доделать.
+            //var x = sharesArray.ToList();
+            //foreach(var m in x)
+            //{
+            //    if(m == null) { x.Remove(m); }
+            //}
+            //sharesArray = x.ToArray();
+            for (int i = 0; i < sharesArray.Length - 1; i++)
+            {
+
+                for (int j = i + 1; j < sharesArray.Length; j++)
+                {
+                    if (sharesArray[j] == null)
+                    {
+                        break;
+                    }
+                    if (sharesArray[i].Percent > sharesArray[j].Percent)
+                    {
+                        temp = sharesArray[i];
+                        sharesArray[i] = sharesArray[j];
+                        sharesArray[j] = temp;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
             string path = "..\\..\\..\\биржа.xml";
             XDocument xDoc = XDocument.Load(path);
-            List<IShare> shares = new List<IShare>();
+            var shares = new List<Share>();
 
             foreach (XElement xRow in xDoc.Element("stocks").Elements("row"))
             {
@@ -56,16 +85,60 @@ namespace DigitalDesign
                     {
                         var share = new Share(boaredAttribute.Value, secidAttribute.Value, shortnameAttribute.Value, decimal.Parse(openAttribute.Value), decimal.Parse(closeAttribute.Value));
                         shares.Add(share);
-                        Console.WriteLine($"Организация: {shortnameAttribute.Value}");
-                        Console.WriteLine($"Тикер: {secidAttribute.Value}");
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        //Console.WriteLine($"Организация: {shortnameAttribute.Value}");
+                        //Console.WriteLine($"Тикер: {secidAttribute.Value}");
+                        //Console.WriteLine();
+                        //Console.WriteLine();
                     }
                 }
             }
-            var (percent, status) = Calculate(12, 14);
 
-            Console.WriteLine($"{Math.Abs(Math.Round(percent, 4)).ToString()}");
+            var sharesArray = new Share[shares.Count];
+            var count = 0;
+
+            foreach(var x in shares)
+            {                
+                var (percent, status) = CalculateShares(x.Open, x.Close);
+                if(status > 0)
+                {
+                    sharesArray[count] = x;
+                    sharesArray[count].Percent = percent;
+                    count++;
+                }
+            }
+
+            // Стандартная сортировка, которая не работает совсем.
+            //shares = sharesArray.ToList();
+            //shares.Sort((x, y) => x.Percent.CompareTo(y.Percent));
+            //var context = from element in shares
+            //              orderby element.Percent 
+            //              select element;
+
+            CustomSortArray(sharesArray);
+            count = 0;
+            for (var x = sharesArray.Length-1; x > -1; x--)
+            {
+                if (sharesArray[x] == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"Компания № {count+1}");
+                    Console.WriteLine(sharesArray[x].SecID);
+                    Console.WriteLine(sharesArray[x].Shortname);
+                    Console.WriteLine($"{Math.Round(sharesArray[x].Percent, 5)} % ");
+                    Console.WriteLine();
+                    count++;
+                    if(count == 10) { break; }
+                } 
+            }
+            //foreach (var x in sharesArray)
+            //{
+            //    if (x == null) { break; }
+            //    Console.WriteLine(x.Percent);
+            //}
+
             Console.ReadKey();
         }
     }
