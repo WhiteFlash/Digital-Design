82c56febf20c9e89c87cd3947b9bcd8b4b26cc5d
whiteFlash <ivankotov375@gmail.com>
2020-08-13 17:09:23 +0300

    Messages are writen to files using API.

--- C#/DigitalDesign/DigitalDesign/Program.cs
+++ C#/DigitalDesign/DigitalDesign/Program.cs
@@ -2,60 +2,76 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Newtonsoft.Json;
 
 namespace DigitalDesign
 {
-    class Program
+    public class FishResponse
     {
-        public static HttpClient https = new HttpClient();
-        public static string responseBody;
-        [HttpGet]
-        public static async void Get() 
+        public string Status { get; set; }
+        public string Text { get; set; }
+        public IEnumerable<string> TextList { get; set; }
+        public void SplitText()
         {
+            var sentences = new List<String>();
+            int position;
+            int start = 0;
 
-            HttpResponseMessage response = await https.GetAsync("https://fish-text.ru/get");
-            response.EnsureSuccessStatusCode();
-            responseBody = await response.Content.ReadAsStringAsync();
-
-            Console.WriteLine(responseBody);
-        }
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Куда хотите сохранить файл? Введите путь");
-            string path = Console.ReadLine();
-            Console.WriteLine("Сколько файлов должно быть сгенерировано?");
-            int quantity = int.Parse(Console.ReadLine());
-
-            Random rand = new Random();
-
-            // Hard codding way to insert text to .txt files
-            //List<string> text = new List<string>() { "Hello world!", "How are you?" };
-
-            Get();
-            List<string> result = responseBody.Split('\t').ToList();
-            try
+            do
             {
-                for (int i = 1; i <= quantity; i++)
+                position = this.Text.IndexOf('.', start);
+                if (position >= 0)
                 {
-                    int x = rand.Next(-10, 200);
-                    var filePath = $"{path}\\LightTextFile{x}.txt";
-                    
-                    File.WriteAllLines(filePath, result);
-                }                
+                    sentences.Add(this.Text.Substring(start, position - start + 1).Trim());
+                    start = position + 1;
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            while (position > 0);
 
-            Console.ReadKey();
+            this.TextList = sentences;
         }
+        class Program
+        {
+
+            public static WebClient https = new WebClient();
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Куда хотите сохранить файл? Введите путь");
+                string path = Console.ReadLine();
+                Console.WriteLine("Сколько файлов должно быть сгенерировано?");
+                int quantity = int.Parse(Console.ReadLine());
+                Random rand = new Random();
+
+                //string reply = https.DownloadString("https://fish-text.ru/get");
+
+                try
+                {
+                    for (int i = 1; i <= quantity; i++)
+                    {
+                        string reply = https.DownloadString("https://fish-text.ru/get");
+                        FishResponse fishResponse = JsonConvert.DeserializeObject<FishResponse>(reply);
+                        Thread.Sleep(1500);
+                        int x = rand.Next(-10, 200);
+                        var filePath = $"{path}\\LightTextFile{x}.txt";
+                        fishResponse.SplitText();
+                        File.WriteAllLines(filePath, fishResponse.TextList);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                Console.ReadKey();
+            }
+        }
+
     }
 }
