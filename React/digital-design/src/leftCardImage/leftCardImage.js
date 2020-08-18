5643b0d148d3cbce8ffaa1acbbbd5bc90c886a5b
whiteFlash <ivankotov375@gmail.com>
2020-08-18 14:43:04 +0300

    Card styled. Works in big screen. Coment and author component added and styled.

--- React/digital-design/src/leftCardImage/leftCardImage.js
+++ React/digital-design/src/leftCardImage/leftCardImage.js
@@ -9,17 +9,17 @@ import customImage from '../img/one.jpg'
 function LeftCardImg() {
     return (          
     <div className="d-flex" id="total">
-        <img className="" src={customImage} alt="Card image" id="pictures"/>
-        <div className="d-flex">
-          <h4 className="d-flex">Hello world</h4>
-          <p className="d-flex">
+        <img className="d-flex" src={customImage} alt="Card image" id="pictures"/>
+        <div className="card-color">
+          <h4 className="">Hello world</h4>
+          <p className="">
             Some example text some example text. John Doe is an architect and engineer
             Some example text some example text. John Doe is an architect and engineer
             Some example text some example text. John Doe is an architect and engineer
             Some example text some example text. John Doe is an architect and engineer
             Some example text some example text. John Doe is an architect and engineer  
           </p>
-          {/* <AuthorAndComments></AuthorAndComments> */}
+          <AuthorAndComments></AuthorAndComments>
         </div>
     </div>
     );
