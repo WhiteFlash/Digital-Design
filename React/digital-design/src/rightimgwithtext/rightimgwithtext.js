17cb4e109db28f3b0fb8d481a6a3931f2775475e
whiteFlash <ivankotov375@gmail.com>
2020-08-19 22:25:19 +0300

    Right card is responsive for 800 and more then 1000px screen size.

--- React/digital-design/src/rightimgwithtext/rightimgwithtext.js
+++ React/digital-design/src/rightimgwithtext/rightimgwithtext.js
@@ -11,7 +11,7 @@ import customImage from '../img/two.jpg';
 function Rightimgwithtext() {
     return (   
         <div className="d-flex" id="right">
-            <div className="" >
+            <div id="textcenter" >
                 <h4 className="">Hello world</h4>
                 <p className="">Some example text some example text. John Doe is an architect and engineer</p>         
                 <AuthorAndComments></AuthorAndComments>
